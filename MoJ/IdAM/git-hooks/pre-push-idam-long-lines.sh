9e8488b9f1e69280ccaa5316bc46179fac55d922
Adam Kwiatek <a.kwiatek@kainos.com>
2017-01-11 23:23:25 +0000

    IdAM long lines check hook
    Check whether there are 100+ lines pushed in source code files.

---
+++ MoJ/IdAM/git-hooks/pre-push-idam-long-lines.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+remote="$1"
+url="$2"
+
+z40=0000000000000000000000000000000000000000
+
+while read local_ref local_sha remote_ref remote_sha
+do
+    if [ "$local_sha" = $z40 ]
+    then
+        # Handle delete
+        :
+    else
+        if [ "$remote_sha" = $z40 ]
+        then
+            # New branch, examine all commits
+            range="$local_sha"
+        else
+            # Update to existing branch, examine new commits
+            range="$remote_sha..$local_sha"
+        fi
+
+        # Check for IdAM too long lines
+        git show --no-color --find-copies --find-renames --unified=0 "$local_ref" \
+            | sed --silent --regexp-extended '
+                # for each file
+                /^diff --git /,/^[+-]/! {
+                    # check only given file types
+                    /[.](groovy|java|js)(.j2)?$/I,/^[+-]/! {
+                        # skip metadata
+                        /^[+]{3} /!n
+                        /^[+].{100}./p
+                    }
+                }' \
+            | grep --color=no '.'
+        if [ 0 == $? ]
+        then
+            echo >&2 "Too long lines found in commit $local_ref, not pushing"
+            exit 1
+        fi
+    fi
+done
+
+exit 0
