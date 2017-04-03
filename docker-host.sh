ec496104b605db564d45103fb4a5d983a67f555e
Adam Kwiatek <a.kwiatek@kainos.com>
2017-04-03 09:41:39 +0200

    docker-host.sh

---
+++ docker-host.sh
@@ -0,0 +1,16 @@
+apk update
+apk add bash htop ruby tmux vim zsh
+
+echo exit | zsh /Users/adamkw/workspaces/akwiatek.github.com/dotfiles/setup.sh
+
+zsh
+
+TERM=xterm
+
+# tput cols
+stty columns 270
+# tput lines
+stty rows 75
+
+htop
+
