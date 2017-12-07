cfeb0bc2e161aaff7bac62b86b0c8b1723bcce60
Adam Kwiatek <a.kwiatek@kainos.com>
2017-12-07 10:19:09 +0100

    Smart hooks
    Add a hook which indexes changed JS files in the worktree.

---
+++ Smart/git-hooks/on-worktree-changed.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo 'Index smart-web-2.0'
+cd smart-web-2.0
+ctags --recurse --exclude={bower_components,node_modules,release,tmp}
+cd ..
+
+echo 'Index pro-serv-web-app-v2'
+cd pro-serv-web-app-v2
+ctags --recurse --exclude={libs,node_modules,release,test/jasmine-2.0.2}
+cd ..
+
+echo 'Flush Vim/CtrlP'
+pwd | \
+    sed 's:/:%:g' | \
+    while read d; do
+        2>/dev/null ls -1 ~/.cache/ctrlp/"$d".txt ~/.cache/ctrlp/"$d"%*.txt
+    done | \
+    xargs --no-run-if-empty rm
